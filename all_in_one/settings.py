8bb7943d080b434c40f10312bff442d14d687894
DeeStarks <danielonoja246@gmail.com>
2021-02-12 23:11:17 +0100

    Updated settings.py

--- all_in_one/settings.py
+++ all_in_one/settings.py
@@ -82,23 +82,7 @@ WSGI_APPLICATION = 'all_in_one.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'interview_assignment',
-#         'USER': 'postgres',
-#         'PASSWORD': 'DeeStarks090*',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.config()
